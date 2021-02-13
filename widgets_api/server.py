d6c385e2a6559e4399e2df6327eef11060642fa4
hoang <bgtoanhoa@gmail.com>
2021-02-13 16:18:49 +0700

    ready to deploy

--- widgets_api/server.py
+++ widgets_api/server.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
 from translate import Translator
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
+port = int(os.environ.get("PORT", 5000))
 
 @app.route('/translate', methods=['POST', 'GET'])
 def translate():
@@ -16,3 +18,5 @@ def translate():
 		return jsonify({"translation": translation})
 	else:
 		return 'something went wrong. Try again!'
+
+app.run(host='0.0.0.0', port=port, debug=True)
